6fd832e27a0cffa8d6c16eb073322a6614455c5c
OscarOchoaS <oscar.ochoa4242@gmail.com>
2024-04-14 21:06:16 -0600

    organize functions

--- Pressure and capture/experiment.py
+++ Pressure and capture/experiment.py
@@ -1,4 +1,6 @@
 # Soft Robot data adquisition and capture
+# # Main: Experiment
+
 # By: Oscar Ochoa 
 # March 2024
 
@@ -6,9 +8,9 @@ import time
 import os
 import threading
 import queue
-from camera import init_camera, capture_frame
-from signal_managment import pressure_measurement, digital_output, serial_read, pwm_airpump
-from exports import export_video, export_csv, create_folder
+import camera
+import signal_managment
+import exports
 
 # Threads queues and events
 pwm_sw_event = threading.Event()
@@ -16,7 +18,7 @@ serial_sw_event = threading.Event()
 camera_stop_event = threading.Event()
 serial_data_queue = queue.Queue()
 
-pipeline = init_camera()        #Initialize camera
+pipeline = camera.init_camera()        #Initialize camera
 
 # # # # #
 # Experiment info               
@@ -24,7 +26,7 @@ id = 'Test_AUX10'               # Experiment name
 goal_pressure = 45              # Goal pressure in MPa
 
 date = time.strftime("%d-%m-%y")
-folder_name = create_folder(id, date)
+folder_name = exports.create_folder(id, date)
 # Update file paths to include folder name
 video_file_path = os.path.join(folder_name, f'video_{id}_{date}.h265')
 csv_file_path = os.path.join(folder_name, f'data_{id}_{date}.csv')
@@ -39,21 +41,21 @@ serial_port = "COM3"
 num_samples = 5                 # From pressure sensor (average will be used)
 
 # Open valve before recording (release prexisting pressure)
-digital_output(device_name, channel_valve, True)
+signal_managment.digital_output(device_name, channel_valve, True)
 time.sleep(0.5)  # Wait for 0.5 seconds
-digital_output(device_name, channel_valve, False)
+signal_managment.digital_output(device_name, channel_valve, False)
 
 # Strart threads
 duty_cycle = 0.1 
-pwm_thread = threading.Thread(target=pwm_airpump, args=(duty_cycle, device_name, channel_pump))
-serial_thread = threading.Thread(target=serial_read, args=(serial_port, ))
-camera_thread = threading.Thread(target=capture_frame, args=(video_file_path, pipeline,))
+pwm_thread = threading.Thread(target=signal_managment.pwm_airpump, args=(duty_cycle, device_name, channel_pump))
+serial_thread = threading.Thread(target=signal_managment.serial_read, args=(serial_port, ))
+camera_thread = threading.Thread(target=camera.capture_frame, args=(video_file_path, pipeline,))
 time.sleep(1)
 camera_thread.start()
 serial_thread.start()
 
 # Open valve lock
-digital_output(device_name, channel_lock, True)
+signal_managment.digital_output(device_name, channel_lock, True)
 
 pwm_thread.start()
 
@@ -67,7 +69,7 @@ while True:
 
     # Capture data
     elapsed_time = time.time() - start_time  # Calculate elapsed time
-    pressure = pressure_measurement(device_name, channel_psensor, num_samples)
+    pressure = signal_managment.pressure_measurement(device_name, channel_psensor, num_samples)
     force = serial_data_queue.get()
     print("Current Pressure: ", pressure, " KPa")
     print("Current Force: ", force, " Kg")
@@ -83,12 +85,12 @@ while True:
         serial_sw_event.set()
 
         # Close valve lock
-        digital_output(device_name, channel_lock, False)
+        signal_managment.digital_output(device_name, channel_lock, False)
         print("Target Pressure achieved")
         break
 
 # Turn on the valve again
-digital_output(device_name, channel_valve, True)
+signal_managment.digital_output(device_name, channel_valve, True)
 time.sleep(0.5)
 camera_stop_event.set()
 
@@ -98,5 +100,5 @@ pwm_thread.join()
 serial_thread.join()
 
 # Make exports
-export_video(video_file_path)
-export_csv(csv_file_path, time_list, pressure_list, force_list)
+exports.export_video(video_file_path)
+exports.export_csv(csv_file_path, time_list, pressure_list, force_list)
